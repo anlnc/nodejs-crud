2fb4fb5f2ca205258a9d6ca98e4466544257e631
An Le <an.le@bizzi.vn>
2022-09-15 21:38:41 +0700

    Update code

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -33,41 +33,50 @@ const getAllRecords = async (req, res, next) => {
 };
 
 const createRecord = async (req, res, next) => {
-  const { userId, groupId } = req.user;
-  const { title, content, files = [] } = req.body;
-  const info = files.map((file) => {
-    const { fileName, description } = file;
-    return {
-      fileName,
-      description,
+  try {
+    const { userId, groupId } = req.user;
+    const { title, content, files = [] } = req.body;
+    const info = files.map((file) => {
+      const { fileName, description } = file;
+      return {
+        fileName,
+        description,
+      };
+    });
+    const data = {
+      content,
+      title,
+      user_id: userId,
+      group_id: groupId,
+      info,
     };
-  });
-  const data = {
-    content,
-    title,
-    user_id: userId,
-    group_id: groupId,
-    info,
-  };
-  const { project_id: projectId } = await prisma.projects.create({
-    data: data,
-  });
+    const { project_id: projectId } = await prisma.projects.create({
+      data: data,
+    });
 
-  BPromise.map(
-    files,
-    async (file) => {
-      const { fileName, base64 } = file;
-      await uploadFileToS3({ fileKey: `${projectId}/${fileName}`, base64 });
-    },
-    { concurrency: 4 }
-  );
+    await BPromise.map(
+      files,
+      async (file) => {
+        const { fileName, base64 } = file;
+        await uploadFileToS3({ fileKey: `${projectId}/${fileName}`, base64 });
+      },
+      { concurrency: 4 }
+    );
 
-  const statusCode = status.CREATED;
-  return res.send({
-    code: statusCode,
-    message: status[statusCode],
-    data,
-  });
+    const statusCode = status.CREATED;
+    return res.send({
+      code: statusCode,
+      message: status[statusCode],
+      data,
+    });
+  } catch (error) {
+    const statusCode = status.INTERNAL_SERVER_ERROR;
+    return res.send({
+      code: statusCode,
+      message: JSON.stringify(error.message),
+      data,
+    });
+  }
 };
 
 export default {
